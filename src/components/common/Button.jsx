51198051a1823775ceefb0b8bdee7d5f2abab04b
Aniket Sharma <sharmaaniket5172@gmail.com>
2024-07-02 18:34:23 +0530

    save

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Button({text, url, addclassname}) {
   if (text) {
     return (
-      <a href={url} className={`btn btn-lg ${addclassname?addclassname:''}`}>
-          {text}
-      </a>
+      <Link to={url} className={`btn btn-lg ${addclassname?addclassname:''}`}>{text}</Link>
     )
   }
 
